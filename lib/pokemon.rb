74625cd5989984356d67eee07f9774c9242ef499
Jessica Antunes <jessantunes@protonmail.com>
2021-03-25 10:34:52 -0400

    commit tree 19200fe3bb121dff8a6f36411dfa5940acf38cc0

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+    attr_accessor :name, :type, :db, :id
+    
+
+    def initialize(id: nil, name:, type:, db: )
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(pk_name, pk_type, db)
+        sql = <<-SQL
+          INSERT INTO pokemon (name, type) 
+          VALUES (?, ?)
+        SQL
+
+        name = pk_name
+        type = pk_type
+        
+
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        
+        result = db.execute(sql, id)[0]  
+        new_pokemon = Pokemon.new(id: id, name: result[1], type: result[2], db: db)
+        new_pokemon
+    end
+
 end
